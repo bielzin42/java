a5cf8caa46a0f5823813aafa0fa0a63b8491dfcd
Gabriel Lima <nickspoot@gmail.com>
2020-05-16 02:51:50 -0300

    solving exercises

--- atividades/src/atividades/ex03.java
+++ atividades/src/atividades/ex03.java
@@ -1,13 +1,13 @@
 package atividades;
 
 public class ex03 {
-		
-	public static void main (String [] args) {
-		for (int i = 233; i < 457; i+=3) {
-			
+
+	public static void main(String[] args) {
+		for (int i = 233; i < 457; i += 5) {
+
 			if (i > 299 && i < 401) {
-				i+=2;
-			}		
+				i -= 2;
+			}
 			System.out.println(i);
 		}
 	}
