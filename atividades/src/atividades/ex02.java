78b9251d10ceda54cf3862ec7d002134d7062f5e
Gabriel Lima <nickspoot@gmail.com>
2020-05-15 23:13:32 -0300

    solving errors

--- atividades/src/atividades/ex02.java
+++ atividades/src/atividades/ex02.java
@@ -4,7 +4,7 @@ public class ex02 {
 	
 	public static void main  (String [] args) {
 		
-		for (double i = 1000; i  < 1999; i++) {
+		for (double i = 1000; i  <= 1999; i++) {
 				
 				if (i % 11 == 0 || i % 11 == 5) {
 					System.out.println(i);
